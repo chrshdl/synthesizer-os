cb73c89f52363b9b0f8a5e3f109e50b005ef1e62
chrshdl <cwasilei@gmail.com>
2026-06-21 20:38:21 +0200

    integrate synthesizer v0.1.19

--- package/python-synthesizer/python-synthesizer.mk
+++ package/python-synthesizer/python-synthesizer.mk
@@ -3,7 +3,7 @@
 ################################################################################
 
 # Use the specific version tag
-PYTHON_SYNTHESIZER_VERSION = 0.1.18
+PYTHON_SYNTHESIZER_VERSION = 0.1.19
 PYTHON_SYNTHESIZER_SITE = $(call github,chrshdl,synthesizer,v$(PYTHON_SYNTHESIZER_VERSION))
 
 PYTHON_SYNTHESIZER_LICENSE = MIT
